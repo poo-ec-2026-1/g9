fb972a6d0ee6c18ae50b9339944dfcd2266502ff
joaoz-zzz <joaomachado2@discente.ufg.br>
2026-06-15 12:25:06 -0300

    remoção de comentários para fazer conexão entre backend e front

--- new/src/src/main/java/com/telemetria/controller/TelaYController.java
+++ new/src/src/main/java/com/telemetria/controller/TelaYController.java
@@ -13,7 +13,7 @@ public class TelaYController {
 
     @FXML
     public void initialize() {
-        // Recupera o nome do cliente que está logado no momento
+        
         String nomeCliente = RegistroLoginController.getNomeSalvo();
         
         if (nomeCliente != null && !nomeCliente.isEmpty()) {
@@ -23,31 +23,31 @@ public class TelaYController {
         }
     }
 
-    // Ação do Botão: Meus Sensores Vinculados
+  
     @FXML
     private void verificarMeusSensores(ActionEvent event) {
-        // Pega o login (ID único) do usuário que está logado atualmente
+        
         String usuarioLogado = RegistroLoginController.getUsuarioSalvo();
         
-        // Se por acaso for o administrador testando a conta padrão
+        
         if (usuarioLogado == null && LoginController.cargoLogado.equals("administrador")) {
             usuarioLogado = "adm";
         }
 
-        // Pega a lista global de sensores que o operador cadastrou
+        
         List<String> todosVinculos = RegistroLoginController.getListaSensoresUsuarios();
         StringBuilder meusSensores = new StringBuilder();
 
-        // Filtra para exibir apenas os sensores vinculados a este cliente específico
+        
         for (String vinculo : todosVinculos) {
             if (vinculo.contains("Usuário: " + usuarioLogado + " ")) {
-                // Limpa o prefixo do usuário para ficar mais amigável na tela do cliente
+                
                 String dadosFormatados = vinculo.replace("Usuário: " + usuarioLogado + " | ", "");
                 meusSensores.append("• ").append(dadosFormatados).append("\n");
             }
         }
 
-        // Exibe o resultado com base no que foi encontrado
+        
         if (meusSensores.length() == 0) {
             exibirAlerta("Meus Dispositivos", "Você ainda não possui nenhum sensor instalado ou vinculado à sua conta.");
         } else {
